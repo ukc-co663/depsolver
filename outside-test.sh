011f72ab4149f1c18ae387653a06d469e00d8fca
Radu Grigore <radugrigore@gmail.com>
2019-03-19 10:17:27 +0000

    small

--- outside-test.sh
+++ outside-test.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-if (( $# != 1 )); then
+if (( $# < 1 )); then
   echo "Which repo shall I test?"
   exit 1
 fi
-REPO="https://github.com/ukc-co663/dependency-solver-2019-$1"
 chmod -R +w temp-playground
 rm -rf temp-playground
 cp -r tests temp-playground
 chmod -R -w temp-playground
-docker run -v $PWD/temp-playground:/depsolver/tests rgrig/ukc-co663-depsolver tests/inside-test.sh $REPO
+docker run -v $PWD/temp-playground:/depsolver/tests rgrig/ukc-co663-depsolver tests/inside-test.sh $1 $2
--- tests/inside-test.sh
+++ tests/inside-test.sh
@@ -4,9 +4,14 @@ if (( $# < 1 )); then
   echo "Which git repo shall I test?"
   exit 1
 fi
+REPO="https://github.com/ukc-co663/dependency-solver-2019-$1"
 
-git clone $1 to-test
+echo "REPO $1 $2"
+git clone $REPO to-test
 cd to-test
+if (( $# >= 2 )); then
+  git checkout $2
+fi
 make all
 if (( $? != 0 )); then
   echo "E: 'make all' failed"
@@ -21,8 +26,15 @@ for i in $(seq 0 9); do
   TESTS="unseen-$i $TESTS"
 done
 for T in $TESTS; do
+  echo
   echo "TEST $T"
   TD=$D/$T
-  ( ulimit -t 300 -m 1000000 ; ./solve $TD/repository.json $TD/initial.json $TD/constraints.json > commands.json 2> stderr ) && \
-  $D/judge.py $TD/repository.json $TD/initial.json commands.json $TD/constraints.json
+  $D/limit -m 4000 -c 300 -w 330 -x "./solve $TD/repository.json $TD/initial.json $TD/constraints.json" > commands.json 2> stderr
+  if (( $? )); then
+    echo
+    echo FAIL
+    cat stderr
+  else
+    $D/judge.py $TD/repository.json $TD/initial.json commands.json $TD/constraints.json
+  fi
 done
